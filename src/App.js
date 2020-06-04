c4ad5427f89f19656b2f6cc94e4c234616c4ea0a
jkunggit <jim.kungjm@gmail.com>
2020-06-04 11:25:02 -0400

    protecting new movies form route

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
   }
 
   render() {
+    const { user } = this.state;
     return (
       <React.Fragment>
         <ToastContainer />
@@ -35,13 +36,19 @@ class App extends Component {
             <Route path='/login' component={LoginForm} />
             <Route path='/logout' component={Logout} />
             <Route path='/register' component={RegisterForm} />
-            <Route path='/movies/:id' component={MovieForm} />
+            <Route
+              path='/movies/:id'
+              render={(props) => {
+                if (!user) return <Redirect to='/login' />;
+                return <MovieForm {...props} />;
+              }}
+            />
             <Route path='/rentals' component={Rentals} />
             <Route path='/customers' component={Customers} />
             <Route
               path='/movies'
               exact
-              render={(props) => <Movies {...props} user={this.state.user} />}
+              render={(props) => <Movies {...props} user={user} />}
             />
             <Route path='/not-found' component={NotFound} />
             <Redirect from='/' exact to='/movies' />
